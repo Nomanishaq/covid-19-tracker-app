607825fd234d4c113ac3b58f4e627982fc588509
nomanishaq <nomanishaq251@gmail.com>
2020-07-13 13:20:03 +0500

    updata

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Navbar,Cards,Chart,CountryPicker} from './Components';
 import './App.css';
 import {GetData} from './Api/GetData';
+import {Container} from '@material-ui/core';
+
 
 class App extends React.Component{
   state = {
@@ -17,12 +19,14 @@ class App extends React.Component{
 render(){
   const {data, country} = this.state;
     return(
-      <div>
+      <React.Fragment>
       <Navbar/>
+      <Container>
         <Cards data={data}/>
         <CountryPicker changeCountryHandler={this.changeCountryHandler}/>
         <Chart data={data} country={country}/>  
-      </div>
+      </Container>
+      </React.Fragment>
     )}
 } 
 export default App;
--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,16 +1,50 @@
 import React from 'react';
-import CountUp from 'react-countup'
+import { makeStyles } from '@material-ui/core/styles';
+import CountUp from 'react-countup';
+import style from './Cards.module.css';
+import {Container,Paper} from '@material-ui/core';
 
 export default function Cards({data: {confirmed, recovered, deaths, lastUpdate}}) {    
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(3),
+      width: theme.spacing(30),
+      height: theme.spacing(30),
+    },
+  },
+}));
+const classes = useStyles();
+
     if(!confirmed){
         return 'loading';
     }
+
    return (
-    <div>
-        <h1>Confrim: <CountUp start={0} end={confirmed.value} duration={5.0}></CountUp></h1>
-        <h1>Recovered: <CountUp start={0} end={recovered.value} duration={5.0}></CountUp></h1>
-        <h1>Deaths: <CountUp start={0} end={deaths.value} duration={5.0}></CountUp></h1>
-        <h1>LastUpdate: {new Date(lastUpdate).toDateString()}</h1>
-  </div>
+ 
+ <Container className={style.container}>
+   <h1 className={style.h1}>Covid-19 Tracker</h1>
+   <div className={classes.root}>
+      <Paper className={style.paper} elevation={5} >
+        <h1>confirmed</h1>
+        <CountUp start={0} end={confirmed.value} duration={5.0}></CountUp>
+        </Paper>
+      <Paper className={style.paper} elevation={5} >
+      <h1>recovered</h1>
+        <CountUp start={0} end={recovered.value} duration={5.0}></CountUp>
+      </Paper>
+       <Paper className={style.paper} elevation={5} >
+      <h1>deaths</h1>
+          <CountUp start={0} end={deaths.value} duration={5.0}></CountUp>
+          </Paper>
+     </div>
+       <p>{new Date(lastUpdate).toDateString()}</p>
+     </Container>
+ 
     )
 }
+
